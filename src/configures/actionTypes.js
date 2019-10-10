ab80e473e376d150219adc16fdc76687ef7cf9dc
yuansl <shileiyuan7@outlook.com>
2019-10-10 15:08:34 +0800

    clear

--- src/configures/actionTypes.js
+++
@@ -1,1 +0,0 @@
-export const Receive_Issue = 'Receive_Issue';
--- src/configures/configHistory.js
+++
@@ -1,9 +0,0 @@
-import { createHistory, useBasename } from 'history';
-import { Project_Path } from './constant';
-
-const history = useBasename(createHistory)({
-  basename: Project_Path
-});
-
-export default history;
-
